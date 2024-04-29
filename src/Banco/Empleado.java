a160a091da4f8b22e5f2b94b30727a43297b13ba
AngelsGV <angels.not99@gmail.com>
2024-04-29 21:05:30 +0200

    Initial commit

--- src/Banco/Empleado.java
+++ src/Banco/Empleado.java
@@ -1,20 +1,45 @@
 package Banco;
 
-import java.io.*;
+import java.io.Serializable;
 
-class Empleado implements Serializable {//resuelta 11.9
-    String dni;
-    String nombre;
-    double sueldo;
+public class Empleado implements Serializable {
+    private String dni;
+    private String nombre;
+    private double sueldo;
 
-public Empleado(String dni,String nombre, double sueldo){
-    this.dni = dni;
-    this.nombre = nombre;
-    this.sueldo = sueldo;
+    public Empleado(String dni, String nombre, double sueldo) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        return "dni: '" + dni + ", nombre: '" + nombre + ", sueldo: " + sueldo;
+    }
 }
-@Override
-    public String toString(){
-    return "dni: " + dni + ", nombre: " + nombre + ", dueldo: " + sueldo;
-}
-//Hasta aquí ok. Crear clase empleado.
-}
+
--- src/Banco/Main.java
+++ src/Banco/Main.java
@@ -1,30 +1,4 @@
 package Banco;
-//Crea una aplicación que gestione los empleados
-// de un banco (añadir, eliminar, mostrar, modificar).
-// Para ello se definirá la clase Empleado con los
-// atributos dni, nombre y sueldo.
-// Los empleados se guardarán en un objeto de la clase
-// Lista para objetos de la clase Object y en el
-// fichero empleados.dat.
-// La aplicación cargará en memoria, al arrancar,
-// la lista de empleados desde el archivo empleados.dat
 
 public class Main {
-    public static void main(String[] args) {
-
-        Metodos gestorEmpleados = new Metodos();
-
-        // Cargar empleados desde el archivo
-        gestorEmpleados.cargarEmpleados();
-
-        // Agregar empleados
-        gestorEmpleados.agregarEmpleado(new Empleado("12345678A", "Juan", 2000));
-        gestorEmpleados.agregarEmpleado(new Empleado("98765432B", "María", 2500));
-
-        // Mostrar empleados
-        gestorEmpleados.mostrarEmpleados();
-
-        // Guardar empleados en el archivo
-        gestorEmpleados.guardarEmpleados();
-    }
 }
